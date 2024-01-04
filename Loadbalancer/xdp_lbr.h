11331bab5cb374b1472711dc4316cdaf69f3d4d7
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2024-01-04 16:39:10 +0000

    Update xdp_lbr.h

--- Loadbalancer/xdp_lbr.h
+++ Loadbalancer/xdp_lbr.h
@@ -1,36 +1,3 @@
-#include "vmlinux.h"
-
-#ifndef BPF_MAP_TYPE_RINGBUF
-#define BPF_MAP_TYPE_RINGBUF 27
-#endif
-
-#ifndef BPF_RB_FORCE_WAKEUP
-#define BPF_RB_FORCE_WAKEUP 2
-#endif
-
-#ifndef ETH_P_IP
-#define ETH_P_IP 0x0800
-#endif
-
-#ifndef BPF_ANY
-#define BPF_ANY 0
-#endif
- 
-#ifndef BPF_F_NO_PREALLOC
-#define BPF_F_NO_PREALLOC (1U << 0)
-#endif
-
-/*
-struct xdp_md {
-  __u32 data;
-  __u32 data_end;
-  __u32 data_meta;
-  __u32 ingress_ifindex;
-  __u32 rx_queue_index;
-  __u32 egress_ifindex;
-};
-*/
-
 #define LADDR18(x) (unsigned int)(172 + (18 << 8) + (0 << 16) + (x << 24))
 #define LADDR17(x) (unsigned int)(172 + (17 << 8) + (0 << 16) + (x << 24))
 #define LEND(a, b, c, d) (unsigned int)(a + (b << 8) + (c << 16) + (d << 24))
