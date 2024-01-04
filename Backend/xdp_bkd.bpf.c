d89d7bde1d97e25eb9a180e850691ad30013985f
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2024-01-04 23:19:07 +0000

    Create xdp_bkd.bpf.c

---
+++ Backend/xdp_bkd.bpf.c
@@ -0,0 +1,32 @@
+#include "xdp_bkd.h"
+
+SEC("xdp")
+int dispatchworkload(struct xdp_md *ctx) {
+	void* data_end = (void*)(long)ctx->data_end;
+	void* data = (void*)(long)ctx->data;
+	
+	struct ethhdr* eth = (struct ethhdr*)data;
+	if ((void*)eth + sizeof(struct ethhdr) > data_end)
+		return XDP_ABORTED;
+	if (bpf_ntohs(eth->h_proto) != ETH_P_IP)
+		return XDP_PASS;
+	
+	struct iphdr* iph = (struct iphdr*)((void*)eth + sizeof(struct ethhdr));
+	if ((void*)iph + sizeof(struct iphdr) > data_end)
+		return XDP_ABORTED;	
+	if (iph->protocol != IPPROTO_TCP)
+		return XDP_PASS;
+	
+	struct tcphdr* tcph = (struct tcphdr*)((void*)iph + sizeof(struct iphdr));
+	if ((void*)tcph + sizeof(struct tcphdr) > data_end)
+		return XDP_ABORTED;
+		
+	if ((bpf_ntohl(iph->daddr) == LADDR18(BKX)) && (bpf_ntohl(iph->saddr) & LEND(255, 255, 255, 0) != LADDR18(0))) {
+		iph->daddr = bpf_htonl(LEND(192, 168, 25, 10));
+		bpf_printk("Packet to be forwrded to the backend address %x", LEND(192, 168, 25, 10));
+	}
+		
+	return XDP_PASS;
+}
+
+char _license[] SEC("license") = "GPL";
