6cdc3a7b324e101538dd4d0ec9f914e994f8d409
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2024-01-05 00:12:35 +0000

    Update xdp_lbr.h

--- Loadbalancer/xdp_lbr.h
+++ Loadbalancer/xdp_lbr.h
@@ -28,16 +28,17 @@
 #define BPF_F_NO_PREALLOC (1U << 0)
 #endif
 
-#define LADDR18(x) (unsigned int)(172 + (18 << 8) + (0 << 16) + (x << 24))
+#define LADDR19(x) (unsigned int)(172 + (19 << 8) + (0 << 16) + (x << 24))
 #define LADDR17(x) (unsigned int)(172 + (17 << 8) + (0 << 16) + (x << 24))
 #define LEND(a, b, c, d) (unsigned int)(a + (b << 8) + (c << 16) + (d << 24))
 #define BEND(a, b, c, d) (unsigned int)((a << 24) + (b << 16) + (c << 8) + d)
 
 #define LBR 2
 #define RTR 3
-#define BKX 4
-#define BKY 5
-#define CUR 6
+#define CUR 4
+#define BKX 3
+#define BKY 4
+
 
 /*
 struct xdp_md {
