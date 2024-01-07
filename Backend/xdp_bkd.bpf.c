ad264f043acff4be09b13aa2a3198778f34e05e5
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2024-01-07 00:42:20 +0000

    Update xdp_bkd.bpf.c

--- Backend/xdp_bkd.bpf.c
+++ Backend/xdp_bkd.bpf.c
@@ -3,9 +3,13 @@
 #include <bpf/bpf_endian.h>
 
 static __always_inline uint8_t* uint2quad(uint32_t* numptr) {
-        uint8_t quad[4];
-        memcpy(quad, numptr, 4);
+        uint8_t* quad = (uint8_t*)((void*)numptr);
         return quad;
+}
+
+static __always_inline uint32_t quad2uint(uint8_t quad0, uint8_t quad1, uint8_t quad2, uint8_t quad3) {
+        uint32_t result = quad0 * 256 * 256 * 256 + quad1 * 256 * 256 + quad2 * 256 + quad3;
+        return result;
 }
 
 SEC("xdp")
@@ -29,9 +33,9 @@ int dispatchworkload(struct xdp_md *ctx) {
 	if ((void*)tcph + sizeof(struct tcphdr) > data_end)
 		return XDP_ABORTED;
 		
-	if ((bpf_ntohl(iph->daddr) == VAL19(BKX)) || (bpf_ntohl(iph->daddr) == VAL19(BKY))) {
-		if ((bpf_ntohl(iph->saddr) & QUAD2V(255, 255, 255, 0)) != VAL19(0)) {
-			iph->daddr = bpf_htonl(QUAD2V(192, 168, 25, 10));
+	if ((bpf_ntohl(iph->daddr) == quad2uint(172, 19, 0, BKX)) || (bpf_ntohl(iph->daddr) == quad2uint(172, 19, 0, BKY))) {
+		if ((bpf_ntohl(iph->saddr) & quad2uint(255, 255, 255, 0)) != quad2uint(172, 19, 0, 0)) {
+			iph->daddr = bpf_htonl(quad2uint(192, 168, 25, 10));
 			uint8_t* daddr = uint2quad(&(iph->daddr));
 			bpf_printk("Packet to be forwrded to the backend IP Q1.%u.%u.%u\n", daddr[1], daddr[2], daddr[3]);
 		}
