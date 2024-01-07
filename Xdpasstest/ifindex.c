510b7f5ee5e360cb4c6d9917e8719621e8d50e14
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2024-01-07 23:51:57 +0000

    Update ifindex.c

--- Xdpasstest/ifindex.c
+++ Xdpasstest/ifindex.c
@@ -12,11 +12,10 @@ int main(int argc, char *argv[]) {
 
         unsigned int rc = if_nametoindex(ifname);
         if (rc) {
-    
-            printf("Interface [%s] has index : %d\n", ifname, rc);
+                printf("Interface [%s] has index : %d\n", ifname, rc);
         }
         else {
-            printf("if_nametoindex error: %s\n", strerror(errno));
+                printf("if_nametoindex error: %s\n", strerror(errno));
         }
 
         return rc;
