6fc9d3d9602201ed56c5cc5ccf9bce32dbe04e7a
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2024-01-09 14:13:34 +0000

    Update xdp_lbr.bpf.c

--- Loadbalancer/xdp_lbr.bpf.c
+++ Loadbalancer/xdp_lbr.bpf.c
@@ -16,19 +16,35 @@ struct {
 static __always_inline uint8_t* uint2quad(uint32_t* numptr) {
         uint8_t* quad = (uint8_t*)((void*)numptr);
         return quad;
-}
+};
 
 static __always_inline uint32_t quad2uint(uint8_t quad0, uint8_t quad1, uint8_t quad2, uint8_t quad3) {
         uint32_t result = quad0 * 256 * 256 * 256 + quad1 * 256 * 256 + quad2 * 256 + quad3;
         return result;
-}
+};
 
-static __always_inline int ip_decrease_ttl(struct iphdr *iph) {
+static __always_inline __u16 csum_fold_helper(__u64 csum) {
+    int i;
+#pragma unroll
+    for (i = 0; i < 4; i++) {
+        if (csum >> 16)
+            csum = (csum & 0xffff) + (csum >> 16);
+    }
+    return ~csum;
+};
+
+static __always_inline __u16 iph_csum(struct iphdr *iph) {
+    iph->check = 0;
+    unsigned long long csum = bpf_csum_diff(0, 0, (unsigned int *)iph, sizeof(struct iphdr), 0);
+    return csum_fold_helper(csum);
+};
+
+/* static __always_inline int ip_decrease_ttl(struct iphdr *iph) {
         u32 check = (__force u32)iph->check;
         check += (__force u32)bpf_htons(0x0100);
         iph->check = (__force __sum16)(check + (check >= 0xFFFF));
         return --iph->ttl;
-}
+} */
 
 SEC("xdp")
 int dispatchworkload(struct xdp_md *ctx) {
