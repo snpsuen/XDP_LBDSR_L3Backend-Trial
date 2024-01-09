4f47c8d87acafe937fccb82277b7c5d7ce56b55c
Sunny Paul Suen <66404197+snpsuen@users.noreply.github.com>
2024-01-09 00:09:43 +0000

    Update xdp_lbr.h

--- Loadbalancer/xdp_lbr.h
+++ Loadbalancer/xdp_lbr.h
@@ -65,10 +65,10 @@ static __always_inline __u16 csum_fold_helper(__u64 csum) {
             csum = (csum & 0xffff) + (csum >> 16);
     }
     return ~csum;
-}
+};
 
 static __always_inline __u16 iph_csum(struct iphdr *iph) {
     iph->check = 0;
     unsigned long long csum = bpf_csum_diff(0, 0, (unsigned int *)iph, sizeof(struct iphdr), 0);
     return csum_fold_helper(csum);
-}
+};
